7ce2318a4732d9027cb54291bdd60b27d1e65a8f
Felix Breidenstein <felix.breidenstein@cased.de>
2015-12-27 13:43:51 +0100

    Refactoring

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ URL = "https://eventphone.de/guru2/phonebook?event=32C3&all=1"
 FILENAME = "32c3.vcf"
 
 def create_vcard_dect(nick, number):
-    text = "BEGIN:VCARD\nVERSION:3.0\nN:{nick};;;\nFN:{nick}\nTEL;HOME:{number}\nEND:VCARD\n".format(nick=nick, number=number)
-    return text
-
+    return "BEGIN:VCARD\nVERSION:3.0\nN:{nick};;;\nFN:{nick}\nTEL;HOME:{number}\nEND:VCARD\n".format(nick=nick, number=number)
 
 def main():
     vcard = ""
@@ -21,19 +19,15 @@ def main():
     for person in entrys:
         entry = [e.text for e in person.findAll('td')]
         try:
-            number = entry[0]
-            nick = entry[1]
-            phonetype = entry[2]
-            position = entry[3]
+            number, nick, phonetype, position = entry
             vcard = "{}{}".format(vcard, create_vcard(nick, number))
         except:
             pass
     print("Write to file")
-    text_file = open(FILENAME, "w")
-    text_file.write(vcard)
-    text_file.close()
+    with open(FILENAME, "w") as f:
+        f.write(vcard)
+        f.close()
     print("Finished!")
-
 
 if __name__ == "__main__":
     main()
